f7099156910dab3af8e7b13dd53889066eac138b
M0rre <M0rre@users.noreply.github.com>
2024-10-28 16:05:22 +0100

    dice fix

--- code/dice.py
+++ code/dice.py
@@ -80,65 +80,65 @@ def get_art():
 
   # Neat dice
   dice_1 = """
-     _______
+     _______ 
     /    ● /|
    /_●___ /●|
   |      |●●|
-  |   ●  |●/
-  |______|/
+  |   ●  |●/ 
+  |______|/  
   
   """
   dice_2 ="""
-     _______
+     _______ 
     /● ● ● /|
    /●_●_●_/●|
   |    ● |●●|
-  |      |●/
-  |_●____|/
+  |      |●/ 
+  |_●____|/  
   """
   dice_3 ="""
-     _______
+     _______ 
     /● ● ● /|
    /●_●_●_/●|
   | ●    |  |
-  |   ●  |●/
-  |_____●|/
+  |   ●  |●/ 
+  |_____●|/  
   """
   dice_4 ="""
-     _______
+     _______ 
     / ●    /|
    /____● / |
   | ●  ● |● |
-  |      | /
-  |_●__●_|/
+  |      | / 
+  |_●__●_|/  
   """
   dice_5 ="""
-     _______
+     _______ 
     /● ● ● /|
    /●_●_●_/●|
   |●   ● |● |
-  |  ●   |●/
-  |●___●_|/
+  |  ●   |●/ 
+  |●___●_|/  
   """
   dice_6 ="""
-     _______
+     _______ 
     / ●    /|
    /____● /●|
   | ●  ● |● |
-  | ●  ● |●/
-  |_●__●_|/
+  | ●  ● |●/ 
+  |_●__●_|/  
   """
 
   #Hmm
-  #lines = zip(dice_1.splitlines(), dice_2.splitlines(), dice_3.splitlines(), dice_4.splitlines(), dice_5.splitlines(), dice_6.splitlines())
+  lines = zip(dice_1.splitlines(), dice_2.splitlines(), dice_3.splitlines(), dice_4.splitlines(), dice_5.splitlines(), dice_6.splitlines())
   
-  #for line in lines:
-    #print("   ".join(line))
-
+  for line in lines:
+    print("   ".join(line))
+"""
   dices = dice_1, dice_2, dice_3, dice_4, dice_5, dice_6
   spacing = ' '* 5
   for pieces in zip(*(dice.splitlines() for dice in dices)):
     print(spacing.join(pieces))
 
-  print(f"Dice 1: {dice_1:<20} Dice 2: {dice_2:^11} Dice 3: {dice_3:>15} ") 
+  print(f"Dice 1: {dice_1:<20} Dice 2: {dice_2:^11} Dice 3: {dice_3:>15} ") """
 get_art()
