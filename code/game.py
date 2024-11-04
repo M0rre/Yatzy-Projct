c6c7e8d848a6f0da62d328e81b9f232ba730dda7
M0rre <91602279+M0rre@users.noreply.github.com>
2024-11-04 15:58:23 +0100

    added error handling for dice.py

--- code/game.py
+++ code/game.py
@@ -1,9 +1,7 @@
 #Ta bort denna sen
-import random as rand
-current_dice = []
-for _ in range(6):
-    current_dice.append(rand.randint(1,6))
-print("Rolled dice", current_dice)
+from collections import Counter
+from dice import final_outcome
+
 
 
 
@@ -24,69 +22,16 @@ def scoreboard():
         "Large straight": None,
         "Full House": None,
         "Chance": None,
-        "Yatzy": None,
-    }
+        "Yatzy": None,    }
     return scoreboard
 
-
-
-
-
-def calculate_upper_section(dice, number):
-    return dice.count(number) * number
-
-def calculate_lower_section(dice):
-    pairs = [dice]
-"""
-    #Ones
-    ones_count = 0 
-    for number in current_dice:
-        if number == 1:
-            ones_count += 1  
-    if ones_count > 0:
-        return ones_count
-            
-    #Twoos 
-    twos_count = 0
-    for number in current_dice:
-        if number == 2:
-            twos_count += 1
-    if twos_count > 0:
-        return twos_count
-    #Threes
-    threes_count = 0 
-    for number in current_dice:
-        if number == 3:
-            threes_count += 1
-    if threes_count > 0:
-        return threes_count
-    #Fours
-    fours_count = 0 
-    for number in current_dice:
-        if number == 4:
-            fours_count += 1
-    if fours_count > 0:
-        return fours_count     
-    #Fives
-    fives_count = 0 
-    for number in current_dice:
-        if number == 5:
-            fives_count += 1
-    if fives_count > 0:
-        return fives_count
-    #Six'
-    sixs_count = 0 
-    for number in current_dice:
-        if number == 6:
-            sixs_count += 1
-    if sixs_count > 0:
-        return sixs_count
-"""
-    
-
-
-
-
+def get_options(final_outcome): #This shit is either huge if statement, or I import counter. edit: imported counter
+        
+        # did some research for counters https://docs.python.org/3/library/collections.html#collections.Counter
+        available_categories = []
+        user_category = []
+        #TODO I mean they're pretty much one rule so simplify into one later
+        if scoreboard
 
 #for _ in range(len(score_board)):
 
