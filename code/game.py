3052462017ddaae789a9ab8a2804a1c3905a16e5
M0rre <91602279+M0rre@users.noreply.github.com>
2024-11-06 16:38:26 +0100

    aaaa

--- code/game.py
+++ code/game.py
@@ -1,8 +1,9 @@
-#Ta bort denna sen
 from collections import Counter
-from dice import final_outcome
-
-
+from dice import roll_dice
+from player import get_players
+#from main import current_player z#TODO
+current_player = "a"
+final_outcome = roll_dice()
 
 
 def scoreboard():
@@ -14,6 +15,7 @@ def scoreboard():
         "Fours": None,
         "Fives": None,
         "Sixes": None,
+        "Bonus": None,
         "One Pair": None,
         "Two Pairs": None,
         "Three of a Kind": None,
@@ -25,37 +27,41 @@ def scoreboard():
         "Yatzy": None,    }
     return scoreboard
 
-def get_options(final_outcome): #This shit is either huge if statement, or I import counter. edit: imported counter
+def get_options(final_outcome, current_player): #This shit is either huge if statement, or I import counter. edit: imported counter
         
         # did some research for counters 
         # https://docs.python.org/3/library/collections.html#collections.Counter
         # like by this point ill just type "[what i want to do]()" and it will prob have a function for it, aka found any()
+       
+        players_scoreboards=get_players()
         c = Counter(final_outcome)
         available_categories = []
 
         #TODO I mean they're pretty much one rule so simplify into one later
         
         # TODO Add bonus
+       
         
-        if scoreboard["Ones"] is None and c[1] > 0:
+        
+        if players_scoreboards[current_player]["Ones"] is None and c[1] > 0:
                 available_categories.append("Ones")
-        if scoreboard["Twos"] is None and c[2] > 0:
+        if players_scoreboards[current_player]["Twos"] is None and c[2] > 0:
                 available_categories.append("Twos")
-        if scoreboard["Threes"] is None and c[3] > 0:
+        if players_scoreboards[current_player]["Threes"] is None and c[3] > 0:
                 available_categories.append("Threes")
-        if scoreboard["Fours"] is None and c[4] > 0:
+        if players_scoreboards[current_player]["Fours"] is None and c[4] > 0:
                 available_categories.append("Fours")
-        if scoreboard["Fives"] is None and c[5] > 0:
+        if players_scoreboards[current_player]["Fives"] is None and c[5] > 0:
                 available_categories.append("Fives")
-        if scoreboard["Sixes"] is None and c[6] > 0:
+        if players_scoreboards[current_player]["Sixes"] is None and c[6] > 0:
                 available_categories.append("Sixes")
                 
         # like by this point ill just type "[what i want to do]()" and it will prob have a function for it, aka found "any()"" that way
-        if scoreboard["Pair"] is None and any(c >= 2)
-        
+        if players_scoreboards[current_player]["Pair"] is None and any(c >= 2):
+                available_categories.append("Pair")
+        if players_scoreboards[current_player]["Two Pairs"] is None and sum(1 for value in c.values() if value >= 2) >= 2:
+                available_categories.append("Two Pairs")
 
 #for _ in range(len(score_board)):
 
-score()
-calculate_upper_section()
-calculate_score(category, dice)
+get_options()
--- code/player.py
+++ code/player.py
@@ -90,6 +90,7 @@ def print_scoreboard(players_scoreboards):
         print()
 
 
-
-players_scoreboards = get_players()
-print_scoreboard(players_scoreboards)
+if __name__ == "__main__":
+    players_scoreboards = get_players()
+    print_scoreboard(players_scoreboards)
+    
